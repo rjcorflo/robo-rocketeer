48f88272948b76f0bbc714802d3612945cd140f6
rjcorflo <rj.corchero@gmail.com>
2017-04-29 12:30:40 +0200

    [DEV-TASK] Add rollback task

---
+++ src/Rollback.php
@@ -0,0 +1,63 @@
+<?php
+namespace RJ\Robo\Task\Rocketeer;
+
+/**
+ * Rocketeer Rollback
+ *
+ * ``` php
+ * <?php
+ * // simple execution, rollback to previous release
+ * $this->taskRocketeerRollback()->run();
+ *
+ * // to certain release
+ * $this->taskRocketeerRollback('path/to/my/rocketeer.phar')
+ *      ->toRelease('release')
+ *      ->run();
+ * ?>
+ * ```
+ */
+class Rollback extends Base
+{
+    use CommonDeployOptions {
+        getCommand as traitCommand;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $action = 'rollback';
+
+    /**
+     * @var string
+     */
+    protected $release;
+
+    /**
+     * Select release to rollback to.
+     *
+     * @param string $release Release
+     */
+    public function toRelease($release)
+    {
+        $this->release = $release;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCommand()
+    {
+        $this->option($this->release);
+        return $this->traitCommand();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        $command = $this->getCommand();
+        $this->printTaskInfo('Deploying Application: {command}', ['command' => $command]);
+        return $this->executeCommand($command);
+    }
+}
